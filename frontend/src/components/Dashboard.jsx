fe87b23d9d062e2c877bc4a9e949407f6e609d3b
Cowwl <84736016+Cowwl@users.noreply.github.com>
2023-09-01 10:54:13 +0530

    Update Dashboard.jsx

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,39 +1,59 @@
-import React from 'react';
-import { Div, Text, SideDrawer, Button } from 'react-atomize';
+import React from "react";
+import { Div, Text, Button, Input } from "react-atomize";
 
 const Dashboard = () => {
-  const [showChat, setShowChat] = React.useState(false);
   const [selectedChat, setSelectedChat] = React.useState(null);
+  const [inputText, setInputText] = React.useState("");
 
   const chats = [
     {
-      name: 'Alice',
+      name: "Alice",
       messages: [
-        { sender: 'Alice', text: 'Hi there!' },
-        { sender: 'You', text: 'Hello!' },
-        { sender: 'Alice', text: 'How are you?' },
+        { sender: "Alice", text: "Hi there!" },
+        { sender: "You", text: "Hello!" },
+        { sender: "Alice", text: "How are you?" },
       ],
     },
     {
-      name: 'Bob',
+      name: "Bob",
       messages: [
-        { sender: 'Bob', text: 'Hey!' },
-        { sender: 'You', text: 'Hi Bob!' },
-        { sender: 'Bob', text: 'What\'s up?' },
+        { sender: "Bob", text: "Hey!" },
+        { sender: "You", text: "Hi Bob!" },
+        { sender: "Bob", text: "What's up?" },
       ],
     },
   ];
 
+  React.useEffect(() => {
+    if (chats.length > 0) {
+      setSelectedChat(chats[0]);
+    }
+  }, []);
+
+  const handleSendMessage = () => {
+    if (inputText.trim() !== "") {
+      setSelectedChat((prevSelectedChat) => ({
+        ...prevSelectedChat,
+        messages: [
+          ...prevSelectedChat.messages,
+          { sender: "You", text: inputText.trim() },
+        ],
+      }));
+      setInputText("");
+    }
+  };
+
   return (
-    <Div>
+    <Div d="flex" h="100vh">
       <Div
         d="flex"
         flexDir="column"
         border="1px solid"
         borderColor="gray200"
-        w="250px"
+        w="15%"
+        h="100%"
       >
-        <Text tag="h4" p="1rem" textAlign="center">
+        <Text tag="h4" p="1rem" textAlign="center" textColor="info700">
           Chats
         </Text>
         {chats.map((chat, index) => (
@@ -41,7 +61,6 @@ const Dashboard = () => {
             key={index}
             onClick={() => {
               setSelectedChat(chat);
-              setShowChat(true);
             }}
             w="100%"
             rounded="0"
@@ -54,25 +73,60 @@ const Dashboard = () => {
           </Button>
         ))}
       </Div>
-      <SideDrawer isOpen={showChat} onClose={() => setShowChat(false)}>
-        <Div p="1rem">
-          <Text tag="h4" textAlign="center">
+      <Div flex="1" border="1px solid" borderColor="gray200" w="100%" h="100%">
+        <Div p="1rem" d="flex" flexDir="column" h="100%">
+          <Text tag="h4" textAlign="center" textColor="info700">
             {selectedChat?.name}
           </Text>
-          {selectedChat?.messages.map((message, index) => (
-            <Div key={index} p={{ b: '1rem' }}>
-              <Text
-                tag="span"
-                textSize="caption"
-                textColor={message.sender === 'You' ? 'info700' : 'black'}
-              >
-                {message.sender}
-              </Text>
-              <Text>{message.text}</Text>
-            </Div>
-          ))}
+          <Div overflowY="auto" overflowX ="hidden" flex="1">
+            {selectedChat?.messages.map((message, index) => (
+              <Div key={index} d="flex" flexDir="column" m={{ b: "1rem" }}>
+                {/* handle overflow in the chat bubble */}
+                <Text
+                  p={{ x: "0.5rem", y: "0.2rem" }}
+                  textAlign={message.sender === "You" ? "right" : "left"}
+                  textColor={message.sender === "You" ? "info700" : "black"}
+                >
+                  {message.sender}
+                </Text>
+                {/* // Position the message to the right if it is sent by me */}
+                <Div
+                  d="flex"
+                  m={{ l: message.sender === "You" ? "auto" : "0" }}
+                  overflowWrap={"break-word"}
+                  overflowX={"hidden"}
+                >
+                  <Text
+                    p={{ x: "1rem", y: "0.5rem" }}
+                    w={{ max: "70%" }}
+                    overflowWrap={"break-word"}
+                    overflowX={"hidden"}
+                    bg={message.sender === "You" ? "info700" : "gray200"}
+                    textColor={message.sender === "You" ? "white" : "black"}
+                    rounded="lg"
+                  >
+                    {message.text}
+                  </Text>
+                </Div>
+              </Div>
+            ))}
+          </Div>
+          <Div d="flex" flexDir="row" p="1rem" w="100%" bg="gray300">
+            <Input
+              placeholder="Type a message..."
+              m={{ r: "1rem" }}
+              value={inputText}
+              onChange={(e) => setInputText(e.target.value)}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  handleSendMessage();
+                }
+              }}
+            />
+            <Button onClick={handleSendMessage}>Send</Button>
+          </Div>
         </Div>
-      </SideDrawer>
+      </Div>
     </Div>
   );
 };
